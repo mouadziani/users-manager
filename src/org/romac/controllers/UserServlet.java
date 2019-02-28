026e4b3342de2254e16b665a2488d3f36c7b43f2
Mouad ZIANI <mouad.ziani1997@gmail.com>
2019-02-28 03:22:05 +0100

    feat: adding listing of users

--- src/org/romac/controllers/UserServlet.java
+++ src/org/romac/controllers/UserServlet.java
@@ -1,16 +1,22 @@
 package org.romac.controllers;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.tpw.dao.UserDao;
+import org.tpw.models.User;
+
 /**
  * Servlet implementation class DemoServlet
  */
-@WebServlet("/DemoServlet")
+@WebServlet("/users")
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -24,10 +30,15 @@ public class UserServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("Test");
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try {
+			UserDao userDao = new UserDao();
+			request.setAttribute("users", userDao.getAllUsers());
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
--- src/org/tpw/dao/UserDao.java
+++ src/org/tpw/dao/UserDao.java
@@ -1,5 +1,30 @@
 package org.tpw.dao;
 
-public class UserDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import org.tpw.helpers.Helper;
+import org.tpw.models.User;
+
+public class UserDao {
+	
+	public ArrayList<User> getAllUsers() throws SQLException {
+		ArrayList<User> users = new ArrayList<User>();
+		String sql = "SELECT * FROM users";
+		PreparedStatement preparedStatement = Helper.getConnection().prepareStatement(sql);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while(resultSet.next()) {
+			User user = new User();
+			user.setId(resultSet.getInt("id"));
+			user.setCin(resultSet.getString("cin"));
+			user.setFirstName(resultSet.getString("first_name"));
+			user.setLastName(resultSet.getString("last_name"));
+			user.setEmail(resultSet.getString("email"));
+			user.setPassword(resultSet.getString("password"));
+			users.add(user);
+		}
+		return users;
+	}
 }
